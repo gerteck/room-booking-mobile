47839c268d0d355677e79958442b0119dd357f18
gerteck <changerteck@gmail.com>
2025-02-28 01:02:02 +0800

    Update API and returned types

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { Room, RoomAvailability } from "@/types/Room";
 // Fetch Room Data
 const API_URL = "https://gist.githubusercontent.com/yuhong90/7ff8d4ebad6f759fcc10cc6abdda85cf/raw/463627e7d2c7ac31070ef409d29ed3439f7406f6/room-availability.json";
 
-export const fetchRooms = async (): Promise<Room[]> => {
+export const fetchRooms = async (date: Date): Promise<Room[]> => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) throw new Error('Network response was not ok');
